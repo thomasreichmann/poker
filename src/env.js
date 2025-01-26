233ea4f0ec7da4bb294d543b3895c831549de26f
Thomas Reichmann <thomasalmeidar@gmail.com>
2025-01-26 15:35:55 -0300

    local supabase + local-env stuff

--- src/env.js
+++ src/env.js
@@ -9,6 +9,7 @@ export const env = createEnv({
 	server: {
 		DATABASE_URL: z.string().url(),
 		NODE_ENV: z.enum(["development", "test", "production"]).default("development"),
+		APP_ENV: z.enum(["local", "development", "staging", "production"]).default("development"),
 	},
 
 	/**
@@ -29,6 +30,7 @@ export const env = createEnv({
 	runtimeEnv: {
 		DATABASE_URL: process.env.DATABASE_URL,
 		NODE_ENV: process.env.NODE_ENV,
+		APP_ENV: process.env.APP_ENV,
 		NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
 		NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
 	},
